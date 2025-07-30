e87391303cfbb61861a99f1adbcfca18c39e32b1
Askar Nurbekov <nurbekoff@gmail.com>
2025-07-30 20:10:56 +0000

    temp converter

---
+++ assignment_module_1/src/main.rs
@@ -0,0 +1,12 @@
+
+fn temperature_converter(){
+    println!("Hello, Temperature converter");
+}
+
+fn assignment_two(){
+    println!("Hello, assignment_twor");
+}
+fn main() {
+    //temperature_converter();
+    assignment_two();
+}
---
+++ assignment_module_1/target/debug/assignment_module_1.d
@@ -0,0 +1,1 @@
+/workspaces/systemssummer2025/assignment_module_1/target/debug/assignment_module_1: /workspaces/systemssummer2025/assignment_module_1/src/main.rs
